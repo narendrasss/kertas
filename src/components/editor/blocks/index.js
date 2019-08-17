f3ced9e4ec4c2165df21587ec3e37e3ac21b07a7
Narendras <n.syahrasyad.n@gmail.com>
2019-08-17 12:36:22 -0700

    Added markdown key handlers

--- src/components/editor/blocks/index.js
+++ src/components/editor/blocks/index.js
@@ -1,6 +1,17 @@
 import CodeBlock from './code-block'
 import HeadingOne from './heading-one'
 
+export const types = {
+  '#': 'heading-one',
+  '##': 'heading-two',
+  '###': 'heading-three',
+  '+': 'ul-item',
+  '-': 'ul-item',
+  '*': 'ul-item',
+  '>': 'blockquote',
+  '```': 'code-block'
+}
+
 export default {
   code: CodeBlock,
   'heading-one': HeadingOne
